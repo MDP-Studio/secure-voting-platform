1850a889281582b03f41d8e0351a33cbe74720b1
Zayn <129614128+CodeDoctorZyn@users.noreply.github.com>
2025-10-05 21:35:56 +1100

    feat(auth): issue JWT session_token on login; support sliding refresh (15m)

--- app/auth.py
+++ app/auth.py
@@ -1,11 +1,3 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required, current_user
-from app import db
-from app.models import User
-
-auth = Blueprint('auth', __name__)
-
-@auth.route('/login', methods=['GET', 'POST'])
 from flask import Blueprint, render_template, redirect, url_for, flash, request, make_response, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 from app import db
